67713feb12c5407c995f07c140ffaa10e65207a1
LuisAlberto2002 <79598943+LuisAlberto2002@users.noreply.github.com>
2024-10-22 12:33:25 -0600

    Update Proyecto-IoT.ino

--- Proyecto-IoT/Proyecto-IoT.ino
+++ Proyecto-IoT/Proyecto-IoT.ino
@@ -1,9 +1,23 @@
+//Librerias Necesarias para ejecucion de componentes
+#define RST 21
+#define AO 26
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(AO, OUTPUT);
+  pinMode(RST, OUTPUT);
+  Serial.println("Iniciando recoleccion de datos");
+  //Determinar funcion de los pines
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //Establecer lectura de valores de temperatura y establecer conexion a internet mediante comandos de terminal
+  //Alertas de temperatura mediante terminal
 
 }
+
+void establecer_conexion(String red, String contraseña){}
+
+void activar_Alerta(){}
