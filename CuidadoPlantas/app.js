7fb9a456f4469aed3d7626286e891d7f556e9832
= <=>
2024-11-17 13:57:42 -0600

    Modificacion de errores

--- CuidadoPlantas/app.js
+++ CuidadoPlantas/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
 const port = 3000;
+const rutas = require('./src/rutas/rutas');
 const url = 'mongodb+srv://VulpesBlack:36944757Ara@vbdb.7dcjohk.mongodb.net/VBCompany?retryWrites=true&w=majority';
 
 // Middleware to serve static files
 app.use(express.static('public'));
 app.use(cors());
+app.use('',rutas);
 
 // Routes
 app.get('/', (req, res) => {
@@ -35,3 +38,12 @@ app.listen(port, () => {
 app.use(cors({
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
 }));
+
+
+mongoose.connect(mongoUrl).then(client=>{
+  app.listen(port,()=>{
+      console.log('VBDB ONLINE');
+  })
+}).catch(err=>{
+  console.log('VBDB DISSABLE', err);
+});
