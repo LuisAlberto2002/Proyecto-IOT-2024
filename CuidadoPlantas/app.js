955283fb0976fc89f51edd243b76a6ecdbfc996e
axelleonardo-1 <123020940+axelleonardo-1@users.noreply.github.com>
2024-11-17 20:48:20 -0600

    moongose connection

--- CuidadoPlantas/app.js
+++ CuidadoPlantas/app.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const port = 3000;
 const rutas = require('./src/rutas/rutas');
-const url = 'mongodb+srv://VulpesBlack:36944757Ara@vbdb.7dcjohk.mongodb.net/VBCompany?retryWrites=true&w=majority';
+const urlAxel = 'mongodb+srv://admin:admin@cluster0.dwmdwry.mongodb.net/Cluster0';
 
 // Middleware to serve static files
 app.use(express.static('public'));
 app.use(cors());
-app.use('',rutas);
+app.use('', rutas);
 
 // Routes
 app.get('/', (req, res) => {
@@ -23,27 +23,24 @@ app.get('/dashboard', (req, res) => {
 app.get('/manage', (req, res) => {
   res.sendFile(__dirname + '/public/manage.html');
 });
+
 app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/public/login.html');
+  res.sendFile(__dirname + '/public/login.html');
 });
+
 app.get('/new_account', (req, res) => {
-res.sendFile(__dirname + '/public/signup.html');
+  res.sendFile(__dirname + '/public/signup.html');
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-app.use(cors({
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}));
-
-
-mongoose.connect(mongoUrl).then(client=>{
-  app.listen(port,()=>{
-      console.log('VBDB ONLINE');
+// Start the server after MongoDB connects
+mongoose
+  .connect(urlAxel)
+  .then(() => {
+    console.log('VBDB ONLINE');
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
   })
-}).catch(err=>{
-  console.log('VBDB DISSABLE', err);
-});
+  .catch((err) => {
+    console.log('VBDB DISABLED', err);
+  });
